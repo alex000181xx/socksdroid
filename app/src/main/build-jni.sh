fa005e7424c8c02f5e52a9e93b1312b9e1a7d2bf
alex000181xx <alex000181xx@gmail.com>
2025-07-07 14:07:58 +0300

    Update build-jni.sh

--- app/src/main/build-jni.sh
+++ app/src/main/build-jni.sh
@@ -2,13 +2,13 @@
 
 export NDK_PATH=$ANDROID_SDK_ROOT/ndk/23.1.7779620
 
+echo "Contents of JNI source folder before build:"
+ls -l app/src/main/jni
+
 rm -rf libs
 rm -f libs.zip
 
 $NDK_PATH/ndk-build -C app/src/main/jni
 
-if [ -d "libs" ] && [ "$(ls -A libs)" ]; then
-  zip -r libs.zip libs
-else
-  echo "Warning: libs directory is missing or empty. No zip file created."
-fi
+echo "Contents of libs folder after build:"
+ls -l libs || echo "libs folder not found"
