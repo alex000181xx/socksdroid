7224b4a09cf3aed41f60c82ea16a757817424c5c
alex000181xx <alex000181xx@gmail.com>
2025-07-07 13:42:28 +0300

    Update build-jni.sh

--- app/src/main/build-jni.sh
+++ app/src/main/build-jni.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-rm -rf jniLibs
+export NDK_PATH=$ANDROID_SDK_ROOT/ndk/23.1.7779620
 
-ndk-build
+rm -rf jniLibs
+rm -rf libs
+
+$NDK_PATH/ndk-build -C app/src/main/jni
 
 for p in armeabi-v7a arm64-v8a x86 x86_64; do
-	mv libs/$p/{tun2socks,libtun2socks.so}
-	mv libs/$p/{pdnsd,libpdnsd.so}
+  mv libs/$p/tun2socks libs/$p/libtun2socks.so
+  mv libs/$p/pdnsd libs/$p/libpdnsd.so
 done
 
 mv libs jniLibs
