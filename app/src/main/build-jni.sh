c31f9ada7742b5de68ba67b9a7e6b54665e3e622
alex000181xx <alex000181xx@gmail.com>
2025-07-07 14:00:44 +0300

    Update build-jni.sh

--- app/src/main/build-jni.sh
+++ app/src/main/build-jni.sh
@@ -2,25 +2,6 @@
 
 export NDK_PATH=$ANDROID_SDK_ROOT/ndk/23.1.7779620
 
-rm -rf app/src/main/jniLibs
 rm -rf libs
-rm -rf app/src/main/bin
 
 $NDK_PATH/ndk-build -C app/src/main/jni
-
-mkdir -p app/src/main/jniLibs
-mkdir -p app/src/main/bin
-
-for abi in armeabi-v7a arm64-v8a x86 x86_64; do
-  mkdir -p app/src/main/jniLibs/$abi
-  mkdir -p app/src/main/bin/$abi
-
-  if [ -d "libs/$abi" ]; then
-    # نسخ ملفات المكتبات .so فقط إلى jniLibs
-    find libs/$abi -maxdepth 1 -name "*.so" -exec cp {} app/src/main/jniLibs/$abi/ \;
-
-    # نسخ الملفات التنفيذية كما هي إلى bin
-    cp libs/$abi/tun2socks app/src/main/bin/$abi/ || true
-    cp libs/$abi/pdnsd app/src/main/bin/$abi/ || true
-  fi
-done
